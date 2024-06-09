8fcfafcedd0e228f4fbd6593f486de888a729af9
kanenosuke-github <shun.216.nosuke@gmail.com>
2024-06-09 21:48:58 +0900

    laravel-todo

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use App\Models\Goal;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TodoController extends Controller
+{
+    
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Goal $goal) {
+        
+        $request->validate([
+            'content' => 'required',
+        ]);
+
+        $todo = new Todo();
+        $todo->content = $request->input('content');
+        $todo->user_id = Auth::id();
+        $todo->goal_id = $goal->id;
+        $todo->done = false;
+        $todo->save();        
+
+        return redirect()->route('goals.index');
+    }
+
+    
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Goal $goal, Todo $todo) {
+        $request->validate([
+            'content' => 'required',
+        ]);
+
+        $todo->content = $request->input('content');
+        $todo->user_id = Auth::id();
+        $todo->goal_id = $goal->id;
+        $todo->done = $request->boolean('done', $todo->done);
+        $todo->save();
+
+        return redirect()->route('goals.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Goal $goal, Todo $todo)
+    {
+        $todo->delete();
+ 
+         return redirect()->route('goals.index');        
+     
+    }
+}
